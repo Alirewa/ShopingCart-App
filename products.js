f67344e6014a106dedbc8db062236799139d699a
Alireza Pourgholam <Alireza.pourgholam444@gmail.com>
2024-05-05 01:11:35 +0330

    added

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Product One",
+    price: 10.99,
+    imageUrl: "./images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "Product two",
+    price: 14.99,
+    imageUrl: "./images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "Product tree",
+    price: 7.99,
+    imageUrl: "./images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "Product four",
+    price: 11.99,
+    imageUrl: "./images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "Product five",
+    price: 4.99,
+    imageUrl: "./images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "Product six",
+    price: 23.99,
+    imageUrl: "./images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "Product seven",
+    price: 42.99,
+    imageUrl: "./images/product-7.jpeg",
+  },
+  {
+    id: 8,
+    title: "Product eight",
+    price: 24.99,
+    imageUrl: "./images/product-8.jpeg",
+  },
+];
